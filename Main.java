3ae05d72ee8f5a5ed4589642a41f8c11432e0dfe
Steph St <steph.streete@outlook.com>
2021-02-18 22:22:44 +0000

    added comments

--- Main.java
+++ Main.java
@@ -3,10 +3,14 @@ class Main {
   public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
+    
+    /**request input of 1st and 2nd*/
     System.out.print("Input your 1st message: ");
     String msg01 = input.nextLine();
     System.out.print("Input your 2nd message: ");
+
     String msg02 = input.nextLine();
+    /**print out 2 sentences joined */
     System.out.println(msg01 + " " + msg02);
   }
 }
